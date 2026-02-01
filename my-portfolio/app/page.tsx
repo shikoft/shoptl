1c4bc8cea2f1ae7e7e94c8f42eab29416ae06a28
shikoft <01222518339a@gmail.com>
2026-02-01 15:25:28 +0700

    Update page.tsx

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -6,14 +6,12 @@ import WorksSection from "../components/Works/WorksSection";
 import Contact from "../components/Contact";
 //import Products from "../components/Products"
 import Footer from "../components/Footer";
-import SectionTabs from "../components/SectionTabs";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main className="min-h-screen pt-24">
-        <Tabs />
       </main>
       <Hero />
       <About />
@@ -26,6 +24,7 @@ export default function Home() {
   );
 
 }
+
 
 
 
