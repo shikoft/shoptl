f9795831c25620e88372e72d6100d59b33cf9c68
shikoft <01222518339a@gmail.com>
2026-02-01 17:29:52 +0700

    Update page.tsx

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,29 +1,29 @@
 import Navbar from "../components/Navbar";
-import SectionTabs from "../components/SectionTabs";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import SectionTabs from "../components/SectionTabs";
 import Contact from "../components/Contact";
-//import Products from "../components/Products"
 import Footer from "../components/Footer";
+//import Products from "../components/Products"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen pt-24">
+
+      <main className="pt-16">
+        <Hero />
+        <About />
+        <SectionTabs />
+        {/* <Products /> */}
+        {/*<WorksSection /> */}
+        <Contact />
       </main>
-      <Hero />
-      <About />
-      {/* <Products /> */}
-      {/*<WorksSection /> */}
-      <SectionTabs />
-      <Contact />
+
       <Footer />
     </>
   );
-
 }
-
 
 
 
