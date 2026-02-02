6fe0dc91907a8c085eb7257d65e30ff05acf5de9
shikoft <01222518339a@gmail.com>
2026-02-02 15:11:12 +0700

    Update page.tsx

--- my-portfolio/app/projects/[slug]/page.tsx
+++ my-portfolio/app/projects/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
-import { projects } from "@/data/projects";
+import { projects } from "../data/projects";
 
 type Props = {
   params: {
@@ -47,3 +47,4 @@ export default function ProjectDetailPage({ params }: Props) {
     </main>
   );
 }
+
