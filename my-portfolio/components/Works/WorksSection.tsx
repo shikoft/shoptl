8fd9d34c92241c3ca694d0a4bd7052284454284c
shikoft <01222518339a@gmail.com>
2026-02-02 14:01:35 +0700

    Update WorksSection.tsx

--- my-portfolio/components/Works/WorksSection.tsx
+++ my-portfolio/components/Works/WorksSection.tsx
@@ -3,46 +3,41 @@ import WorkCard from "./WorkCard";
 const works = [
   {
     title: "Project A",
+    slug: "project-a",
     image: "/product1.png",
     desc: "Motion Graphics / Visual",
+    type: "internal",
   },
   {
     title: "Project B",
+    slug: "project-b",
     image: "/product2.png",
     desc: "Creative Video",
+    type: "internal",
   },
   {
-    title: "Project C",
+    title: "Project Link (External)",
     image: "/product3.png",
-    desc: "Motion Graphics / Visual",
+    desc: "Landing Page",
+    type: "external",
+    url: "https://www.facebook.com/Thu.79.non",
   },
   {
     title: "Project D",
+    slug: "project-d",
     image: "/product4.png",
     desc: "Creative Video",
-  },
-  {
-    title: "Project E",
-    image: "/product5.png",
-    desc: "Game",
+    type: "internal",
   },
 ];
 
 export default function WorksSection() {
   return (
-    <section
-      id="projects"
-      className="
-        bg-white
-        py-24
-      "
-    >
-      {/* TITLE */}
+    <section id="projects" className="bg-white py-24">
       <h2 className="text-4xl font-bold text-center text-cyan-500 mb-14">
         PROJECTS
       </h2>
 
-      {/* GRID */}
       <div
         className="
           max-w-6xl mx-auto px-6
