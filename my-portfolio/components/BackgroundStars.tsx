d59db15e9891f519b48dee6ebb3d05aad4b9117f
shikoft <01222518339a@gmail.com>
2026-02-04 23:14:02 +0700

    Delete my-portfolio directory

--- my-portfolio/components/BackgroundStars.tsx
+++
@@ -1,9 +0,0 @@
-export default function BackgroundStars() {
-  return (
-    <div className="stars-bg">
-      {Array.from({ length: 8 }).map((_, i) => (
-        <span key={i} className="shooting-star" />
-      ))}
-    </div>
-  );
-}
--- my-portfolio/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-gray-500">
-      © 2026 ShikoFT. All rights reserved.
-    </footer>
-  );
-
-}
